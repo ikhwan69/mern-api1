13ddbc139c22c1f6144db9ec18b67458ffdb1abf
ikhwan69 <ikhwanrahman76@gmail.com>
2020-12-03 06:44:59 +0700

    Standard struktur folder

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 
 const app = express();
+const router = express.Router();
 
-app.use(() => {
-    console.log("Membuat server");
+router.use('/products', (req, res, next) => {
+    // console.log('url : ',req.originalUrl);
+    // console.log('method : ',req.method);
+    res.json({name: "Ikhwanul Rahman", email: "ikhwanrahman76@gmail.com"});
+    next();
 })
 
+router.use('/price', (req, res, next) => {
+    res.json({price: 3000});
+    next();
+})
+
+router.get('/customer', (req, res, next) => {
+    res.json({title:"Customer"});
+    next();
+})
+
+app.use('/', router)
+
+// GET '/users' ===> [{name: ikhwan}]
 app.listen(4000);
